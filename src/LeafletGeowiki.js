69f0be88a1ce84b4845dfc17d9d99fe33f4b2ff7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-07-17 07:48:13 +0200

    Emit 'renderTemplate'

--- src/LeafletGeowiki.js
+++ src/LeafletGeowiki.js
@@ -395,6 +395,7 @@ class LeafletGeowiki {
         metersPerPixel: 40075016.686 * Math.abs(Math.cos(this.map.getCenter().lat / 180 * Math.PI)) / Math.pow(2, this.map.getZoom() + 8)
       }
     }
+    this.emit('renderTemplate', data)
 
     const p = t.render(data)
 
